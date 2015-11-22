1aaaff82117cd7b99205959713bb6fc8c98b7a9d
Prashanth818 <vdev818@aucklanduni.ac.nz>
2015-11-22 19:43:00 +1300

    Hello World module. Can be loaded and unloaded.

---
+++ .HelloWorld.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/prashanth/Eudyptula_Challenge/HelloWorld.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-68-generic/scripts/module-common.lds --build-id  -o /home/prashanth/Eudyptula_Challenge/HelloWorld.ko /home/prashanth/Eudyptula_Challenge/HelloWorld.o /home/prashanth/Eudyptula_Challenge/HelloWorld.mod.o
---
+++ .tmp_versions/HelloWorld.mod
@@ -0,0 +1,2 @@
+/home/prashanth/Eudyptula_Challenge/HelloWorld.ko
+/home/prashanth/Eudyptula_Challenge/HelloWorld.o
